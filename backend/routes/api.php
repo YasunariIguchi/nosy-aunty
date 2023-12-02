7a2531b32930affeb670b83d7d8483b19c8115fd
YasunariIguchi <iguchi.yasunari.0219@gmail.com>
2023-12-02 09:51:18 +0900

    breezeのインストール(API only / PHPUnit)

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,18 +11,16 @@ use App\Http\Controllers\ConversationController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
 Route::get('list',[ListController::class, 'index']);
-
-/*
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-*/
+
 // conversationのCRUD
 Route::post('conversation', [ConversationController::class, 'create']);
 Route::get('conversation', [ConversationController::class, 'fetch']);
