5d5c8694c8a0c725c9db08d356ab4cfbcd971ee6
Yamazaki Takeshi <yamazaki@uhc.jp>
2023-12-03 21:53:11 +0900

    [add] Styled JSXを使用したスタイルの適用

--- frontend/src/pages/Top.js
+++ frontend/src/pages/Top.js
@@ -5,11 +5,20 @@ import PasteButton from '../components/PasteButton';
 export default function Top() {
   return (
     <>
+      <style jsx>{`
+        .paste-button {
+          margin-bottom: 1rem;
+          display: flex;
+          justify-content: flex-end;
+        }
+      `}</style>
       <CssBaseline />
       <Container maxWidth="md">
         <h1>おせっかいおばさん</h1>
         <p>あんたまた出会い系で女に振られたんやて！？ちょっとスマホでどんなやり取りしてんの！？見せてみーや！</p>
-        <PasteButton />
+        <div className="paste-button">
+          <PasteButton />
+        </div>
         <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
       </Container>
     </>
