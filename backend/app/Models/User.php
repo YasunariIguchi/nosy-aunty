af3941a14fd02eda12300209855e17802009878a
NOBU <nobunobupc95@gmail.com>
2023-12-10 23:42:38 +0900

    coversationsの上に行追加

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-
+    
     public function conversations(): HasMany
     {
         return $this->hasMany(Conversation::class);
