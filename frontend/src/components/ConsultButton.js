a8fe52e4e271607b30754953eeae36dfe4ec3059
Yamazaki Takeshi <yamazaki@uhc.jp>
2023-12-17 12:26:29 +0900

    [add] ボタンの二度押し禁止

--- frontend/src/components/ConsultButton.js
+++ frontend/src/components/ConsultButton.js
@@ -1,9 +1,12 @@
 import { Button } from "@mui/material";
 import axios from "axios";
+import { useState } from "react";
 export default function ConsultButton({ inputTextRef, typeStart, handleSpinner }) {
+  const [isDisabled, setIsDisabled] = useState(false);
 
   const fetchResult = () => {
     handleSpinner(true);
+    setIsDisabled(true);
     typeStart(" ");
     const requestBody = {
       line: inputTextRef.current.value,
@@ -19,17 +22,19 @@ export default function ConsultButton({ inputTextRef, typeStart, handleSpinner }
         .then((res) => {
           console.log(res);
           handleSpinner(false);
+          setIsDisabled(false);
           typeStart(res.data.advice);
       });
     } catch (e) {
       handleSpinner(false);
+      setIsDisabled(false);
       typeStart("Error occurred.");
       return e;
     }
   }
 
   return (
-    <Button fullWidth variant="contained" onClick={fetchResult} >
+    <Button fullWidth variant="contained" onClick={fetchResult} disabled={isDisabled} >
       おばちゃんに見せてアドバイスをもらう
     </Button>
   );
