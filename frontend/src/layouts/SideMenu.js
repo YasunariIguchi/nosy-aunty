e12c60675d19c0d380f87415f0d3892ff91d5af8
YasunariIguchi <iguchi.yasunari.0219@gmail.com>
2023-12-24 12:21:34 +0900

    会話履歴リンクはログイン時のみ表示

--- frontend/src/layouts/SideMenu.js
+++ frontend/src/layouts/SideMenu.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Drawer, List, ListItem, ListItemText } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-const SideMenu = ({ isMenuOpen, setIsMenuOpen }) => {
+const SideMenu = ({ isMenuOpen, setIsMenuOpen, isLoggedIn }) => {
   // サイドメニューの項目やクリック時の処理をここに追加
 
   return (
@@ -15,9 +15,11 @@ const SideMenu = ({ isMenuOpen, setIsMenuOpen }) => {
         <ListItem button component={Link} to="/">
           <ListItemText primary="トップ" />
         </ListItem>
-        <ListItem button component={Link} to="/conversation">
-          <ListItemText primary="会話履歴" />
-        </ListItem>
+        {isLoggedIn && ( // ログインしている場合のみ会話履歴を表示
+          <ListItem button component={Link} to="/conversation">
+            <ListItemText primary="会話履歴" />
+          </ListItem>
+        )}
         {/* 他のメニュー項目を追加 */}
       </List>
     </Drawer>
