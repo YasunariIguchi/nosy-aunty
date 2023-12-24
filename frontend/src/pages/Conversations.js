05c7db20f2d7a8cc33956c263f467ed2f9ebeb5d
YasunariIguchi <iguchi.yasunari.0219@gmail.com>
2023-12-24 13:00:25 +0900

    table表示、投稿日時で降順、時刻をJST表示

--- frontend/src/pages/Conversations.js
+++ frontend/src/pages/Conversations.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
 
 const ConversationList = () => {
   const [conversationList, setConversationList] = useState([]);
@@ -15,28 +16,52 @@ const ConversationList = () => {
       // 他の必要なオプションがあればここに追加
     })
       .then((response) => {
-        setConversationList(response.data); // レスポンスから会話履歴のデータをセットする
+        // 時刻をJSTに変換する処理
+        const convertedData = response.data.map((conversation) => {
+          const date = new Date(conversation.created_at);
+          // UTCから日本時間に変換（9時間追加）
+          const jstDate = new Date(date.getTime());
+          return { ...conversation, created_at: jstDate.toLocaleString('ja-JP', { timeZone: 'Asia/Tokyo' }) };
+        });
+        // 投稿日時で降順ソート
+        const sortedData = convertedData.sort((a, b) => {
+          return new Date(b.created_at) - new Date(a.created_at);
+        });
+        setConversationList(sortedData);
       })
       .catch((error) => {
         console.error('会話履歴の取得に失敗しました:', error);
       });
   }, []); // ページ遷移時に1度だけ実行される
 
+  const truncateString = (str, maxLength) => {
+    return str.length > maxLength ? str.substring(0, maxLength) + '...' : str;
+  };
+
+
   return (
-    <div>
-      <h2>会話履歴一覧</h2>
-      <ul>
-        {conversationList.map((conversation) => (
-          <li key={conversation.id}>
-            <p>投稿日時: {conversation.created_at}</p>
-            <p>投稿内容: {conversation.line}</p>
-            <p>おばちゃんのアドバイス: {conversation.advice}</p>
-            {/* 他の表示したい情報があればここに追加 */}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>投稿日時</TableCell>
+            <TableCell>投稿内容</TableCell>
+            <TableCell>おばちゃんのアドバイス</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {conversationList.map((conversation) => (
+            <TableRow key={conversation.id}>
+              <TableCell>{truncateString(conversation.created_at)}</TableCell>
+              <TableCell>{truncateString(conversation.line, 20)}</TableCell>
+              <TableCell>{truncateString(conversation.advice, 20)}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
+
 };
 
 export default ConversationList;
