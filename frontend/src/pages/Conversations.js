f0d12d25c543c09213d8ada466bdccbe1820b44d
YasunariIguchi <iguchi.yasunari.0219@gmail.com>
2023-12-24 16:24:38 +0900

    表を見やすく

--- frontend/src/pages/Conversations.js
+++ frontend/src/pages/Conversations.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TablePagination, Paper, Typography, Button } from '@mui/material';
+import { Table, TableBody, TableCell, tableCellClasses,  TableContainer, TableHead, TableRow, TablePagination, Paper, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import './ConversationList.css';
 
 const columns = [
   { id: 'datetime', label: '投稿日時', minWidth: 100 },
@@ -9,6 +11,26 @@ const columns = [
   { id: 'reply', label: 'おばちゃんのアドバイス', minWidth: 100 },
   { id: 'detail', label: '詳細', minWidth: 100, align: 'center' },
 ];
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
 
 const ConversationList = () => {
   const [conversationList, setConversationList] = useState([]);
@@ -65,51 +87,53 @@ const ConversationList = () => {
 
 
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-      <Typography variant="h5" gutterBottom>
-        おばちゃんとの会話履歴
-      </Typography>
-      <TableContainer sx={{ maxHeight: 480 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  align={column.align}
-                  style={{ minWidth: column.minWidth }}
-                >
-                  <b>{column.label}</b>
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {getPaginatedData().map((conversation) => (
-              <TableRow key={conversation.id}>
-                <TableCell>{truncateString(conversation.created_at)}</TableCell>
-                <TableCell>{truncateString(conversation.line, 30)}</TableCell>
-                <TableCell>{truncateString(conversation.advice, 30)}</TableCell>
-                <TableCell align="center">
-                  <Button component={Link} to={`/conversation/${conversation.id}`} variant="contained" color="primary">
-                    詳細確認
-                  </Button>
-                </TableCell>
+    <div className="conversation-container">
+      <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+        <Typography variant="h5" gutterBottom>
+          おばちゃんとの会話履歴
+        </Typography>
+        <TableContainer sx={{ maxHeight: 480 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <StyledTableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    <b>{column.label}</b>
+                  </StyledTableCell>
+                ))}
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={conversationList.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
+            </TableHead>
+            <TableBody>
+              {getPaginatedData().map((conversation) => (
+                <StyledTableRow key={conversation.id}>
+                  <StyledTableCell>{truncateString(conversation.created_at)}</StyledTableCell>
+                  <StyledTableCell>{truncateString(conversation.line, 30)}</StyledTableCell>
+                  <StyledTableCell>{truncateString(conversation.advice, 30)}</StyledTableCell>
+                  <StyledTableCell align="center">
+                    <Button component={Link} to={`/conversation/${conversation.id}`} variant="contained" color="primary">
+                      詳細確認
+                    </Button>
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={conversationList.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+    </div>
   );
 
 };
